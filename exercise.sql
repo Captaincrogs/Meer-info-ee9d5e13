2745247eb13468b1ba166f058a03a764593877f6
Captaincrogs <54841728+Captaincrogs@users.noreply.github.com>
2020-01-10 11:16:23 +0100

    Create exercise.sql

---
+++ exercise.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `sterrenstelsel`.`planeet` 
+ADD COLUMN `planeetcol` VARCHAR(45) NOT NULL AFTER `massa`;
+ADD COLUMN `planeetcol` VARCHAR(45) NOT NULL AFTER `Afstand_tot_de_zon`;
+ADD COLUMN `planeetcol` VARCHAR(45) NOT NULL AFTER `diameter`;
+ADD COLUMN `planeetcol` VARCHAR(45) NOT NULL AFTER `zon`;
+ADD COLUMN `planeetcol` VARCHAR(45) NOT NULL AFTER `massa`;
+
